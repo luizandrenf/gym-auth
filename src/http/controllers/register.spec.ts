c8638cd08c8ca6001424f73301c13acecbbc35d3
luizandrenf <luizandrenf@gmail.com>
2024-06-28 13:16:33 -0400

    test: test e2e of register controller

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,2 +1,23 @@
-import { test } from 'vitest'
-test('ok', async () => {})
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const respose = await request(app.server).post('/users').send({
+      name: 'Jhon Doe',
+      email: 'jhondoe@gmail.com',
+      password: '123456',
+    })
+
+    expect(respose.statusCode).toEqual(201)
+  })
+})
